5f9241f0359b5461a570c6dd994e92f69c59337d
trayanus1026 <trayanus929@gmail.com>
2024-02-22 19:09:16 +0100

    mobile menu repair

--- src/app/(frontpages)/_pageComponents/Header/Header.tsx
+++ src/app/(frontpages)/_pageComponents/Header/Header.tsx
@@ -16,7 +16,7 @@ const PoppinsFont = Poppins({
 
 const HeaderRoot = tw.header`
     dark:bg-primary bg-white    
-    py-11 px-4
+    py-3 md:py-11 px-2 md:px-4
 `;
 
 const HeaderWrapper = tw.div`    
@@ -41,9 +41,9 @@ const Header = function () {
     }
 
     return (
-        <HeaderRoot className="py-3 md:py-11">
+        <HeaderRoot>
             <div className="flex container mx-auto max-w-8xl justify-between">
-                <Link href={'/'} className="mx-3 flex-none">
+                <Link href={'/'} className="flex-none">
                     <Image
                         src={`/logo-dark.svg`}
                         alt="EcoRise"
@@ -59,7 +59,7 @@ const Header = function () {
                         className="dark:hidden block"
                     />
                 </Link>
-                <Navigation className="flex-auto flex 2xl:justify-end">
+                <Navigation className="flex-auto flex 2xl:justify-end ">
                     <NavList className="justify-between gap-5 md:gap-14 mx-auto flex-wrap">
                         <NavItem className="tooltip relative flex flex-col items-center group hidden 2xl:block">
                             <Link href={'/tokenize'} className={`${PoppinsFont.className} dark:text-white text-primary text-sm md:text-xl hover:font-medium dark:hover:text-textColor-100`}>Tokenize</Link>
@@ -91,30 +91,36 @@ const Header = function () {
                     </NavList>
                 </Navigation>
                 <Button className="w-[15rem] my-5 mx-3 flex-auto flex 2xl:justify-end hidden 2xl:block" variant="gradient" fontFamily="Poppins" rounded="xl3" value={btnText} />
-                <label className="z-40 cursor-pointer mr-5 px-0 py-6 flex-right flex 2xl:justify-end block 2xl:hidden" htmlFor="mobile-menu">
-                    <input onClick={() => setNav(!nav)} className="peer hidden" type="checkbox" id="mobile-menu" />
-                    <div className="relative z-50 block h-[1px] w-7 bg-white bg-transparent content-[''] before:absolute before:top-[-0.35rem] before:z-50 before:block before:h-full before:w-full before:bg-white before:transition-all before:duration-200 before:ease-out before:content-[''] after:absolute after:right-0 after:bottom-[-0.35rem] after:block after:h-full after:w-full after:bg-white after:transition-all after:duration-200 after:ease-out after:content-[''] peer-checked:bg-transparent before:peer-checked:top-0 before:peer-checked:w-full before:peer-checked:rotate-45 before:peer-checked:transform after:peer-checked:bottom-0 after:peer-checked:w-full after:peer-checked:-rotate-45 after:peer-checked:transform">
+                <div onClick={() => setNav(!nav)} className="flex-right mt-15 2xl:justify-end block 2xl:hidden">
+                    <Image
+                        src={`/mobilemenuwhite.png`}
+                        alt="EcoRise"
+                        width={36}
+                        height={36}
+                        className="pt-8 dark:block hidden"
+                    />
+                    <Image
+                        src={`/mobilemenublack.png`}
+                        alt="EcoRise"
+                        width={36}
+                        height={36}
+                        className="pt-8 dark:hidden block"
+                    />
+                </div>
+                {nav && (
+                    <div onClick={() => setNav(!nav)} className="fixed top-40 right-0 z-40">
+                        <div className="dark:bg-primary bg-white px-6 py-6 opacity-100 border dark:border-white border-black border-1">
+                            <menu className="text-3xl ">
+                                <li className=""><Link href={'/tokenize'} className={`${PoppinsFont.className} dark:text-white text-primary`}>Tokenize</Link></li>
+                                <li className="pt-6"><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary`}>Launchpad</Link></li>
+                                <li className="pt-6"><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary`}>Marketplace</Link></li>
+                                <li className="pt-6"><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary`}>Dashboard</Link></li>
+                                <li className="pt-6"><Link href={'/about-us'} className={`${PoppinsFont.className} dark:text-white text-primary`}>AboutUs</Link></li>
+                                <li className="pt-6"><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary`}>{btnText}</Link></li>
+                            </menu>
+                        </div>
                     </div>
-                    {nav && (
-                        <div onClick={() => setNav(!nav)} className="fixed inset-0 z-40 hidden h-full w-full bg-white/50 backdrop-blur-sm peer-checked:block">
-                            &nbsp;
-                        </div>
-                    )}
-                    {nav && (
-                        <div className="fixed top-0 right-0 z-40 h-full w-full translate-x-full overflow-y-auto overscroll-y-none transition duration-500 peer-checked:translate-x-0">
-                            <div className="float-right min-h-full max-w-92 bg-black px-6 pt-12 shadow-2xl opacity-70">
-                                <menu className="mt-20">
-                                    <li><Link href={'/tokenize'} className={`${PoppinsFont.className} dark:text-white text-primary text-4xl`}>Tokenize</Link></li>
-                                    <li><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary text-4xl`}>Launchpad</Link></li>
-                                    <li><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary text-4xl`}>Marketplace</Link></li>
-                                    <li><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary text-4xl`}>Dashboard</Link></li>
-                                    <li><Link href={'/about-us'} className={`${PoppinsFont.className} dark:text-white text-primary text-4xl`}>AboutUs</Link></li>
-                                    <li><Link href={'/'} className={`${PoppinsFont.className} dark:text-white text-primary text-4xl`}>{btnText}</Link></li>
-                                </menu>
-                            </div>
-                        </div>
-                    )}
-                </label>
+                )}
             </div>
         </HeaderRoot>
     );
